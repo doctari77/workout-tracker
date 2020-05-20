e2d857cb73d92a0fd5aab74c5a936313ff6fce7b
Todd Leslie <spotontechnology@gmail.com>
2020-05-20 09:06:57 -0600

    api work

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,7 @@
-const router = require(Express).Router()
+const router = require("express").Router()
 const Workout =require("../models/workout")
 
-router.get("api/workouts", (req,res)  => {
+router.get("/api/workouts", (req,res)  => {
     Workout.find()
     .then(data => {
         res.json(data)
@@ -11,8 +11,8 @@ router.get("api/workouts", (req,res)  => {
 
 })
 })
-router.post("api/workouts", (req,res) =>{
-    Workout.create(req.body)
+router.post("/api/workouts", (req,res) =>{
+    Workout.create({})
     .then(data=>{
         res.json(data)
     })
@@ -24,5 +24,35 @@ router.put("/api/workouts/:id", ({body,params}, res) =>{
     Workout.findByIdAndUpdate(params.id,
         {$push: {exercises: body}},
         {new: true, runValidators: true}
+        
         )
+        .then(data=>{
+            res.json(data)
+        })
+    .catch(err=>{
+        res.json(err)
+    })
 })
+
+
+router.delete("/api/workouts", ({body}, res) => {
+    Workout.findByIdAndDelete(body.id)
+    .then(() => {
+        res.json(true)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+router.get("/api/workouts/range", (req,res)  => {
+    Workout.find({}).limit(7)
+    .then(data => {
+        res.json(data)
+    })
+.catch(err =>  {
+    res.json(err)
+
+})
+})
+module.exports = router;
